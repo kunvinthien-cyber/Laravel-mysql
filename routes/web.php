1cf2bc4b73a769df1013424089c9ab3f29a440ac
Kun vinthien <kunvinthien@gmail.com>
2026-07-07 12:54:24 +0700

    Add install-system web route

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\BackupController;
 use App\Http\Controllers\SettingController;
-
+use Illuminate\Support\Facades\Artisan;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -93,5 +93,13 @@ Route::middleware(['auth', 'role:admin'])->group(function () {
     Route::get('/settings', [SettingController::class, 'index'])->name('settings.index');
     Route::post('/settings', [SettingController::class, 'update'])->name('settings.update');
 });
+Route::get('/install-system', function () {
+    // ១. បង្កើតតារាង Database និងបញ្ចូលគណនីគំរូទាំងអស់
+    Artisan::call('migrate:fresh --seed --force');
 
+    // ២. បង្កើតតំណភ្ជាប់រូបភាពផលិតផល
+    Artisan::call('storage:link');
+
+    return 'ការដំឡើងប្រព័ន្ធ POS ទទួលបានជោគជ័យ!';
+});
 require __DIR__.'/auth.php';
