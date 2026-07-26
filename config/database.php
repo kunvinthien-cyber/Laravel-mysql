538a1da1d81445fdf1254bb659a4d9c566708a58
Kun vinthien <kunvinthien@gmail.com>
2026-07-26 16:10:20 +0700

    Fix: Correct syntax error in mysql connection PDO options

--- config/database.php
+++ config/database.php
@@ -61,7 +61,10 @@ return [
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 Mysql::ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
+            ]) + [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
+            ] : [],
         ],
 
         'mariadb' => [
